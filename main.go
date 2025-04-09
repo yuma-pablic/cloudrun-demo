11ac249b16570403b62235aae401b158fef1906b
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-04-10 00:03:43 +0900

    feat

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -14,6 +17,22 @@ func main() {
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	connStr := "postgres://admin:admin@localhost:5432/postgres"
+	ctx := context.Background()
+
+	dbpool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer dbpool.Close()
+
+	var greeting string
+	err = dbpool.QueryRow(ctx, "SELECT 'Hello from Postgres!'").Scan(&greeting)
+	if err != nil {
+		log.Fatalf("Query failed: %v\n", err)
+	}
+	fmt.Println(greeting)
 
 	r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		response := map[string]string{"status": "ok"}
@@ -25,6 +44,7 @@ func main() {
 		}
 	})
 
+	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("server failed to start: %v", err)
 	}
